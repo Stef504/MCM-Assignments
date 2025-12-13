30cd3c8f93f343bda647f1c888e32de02afd06fb
Stefania Germena <stefania.germena3@gmail.com>
2025-12-13 01:06:22 +0100

    all is good, we can clean up later

--- MCM_lab2_25_26/src/include/BuildTree.m
+++ MCM_lab2_25_26/src/include/BuildTree.m
@@ -33,53 +33,53 @@ R_12=X(-90)*Z(-90);
 iTj_0(:, :, 2)=[R_12 [0 0 0.110]'; ident];
 
 R_23=Y(90);
-iTj_0(:, :, 3)=[R_23 [0.1 0 0]'; ident];
+iTj_0(:, :, 3)=[R_23 [0.100 0 0]'; ident];
 
 R_34=Y(-90);
-iTj_0(:, :, 4)=[R_34 [0 0 0.095]'; ident];
+iTj_0(:, :, 4)=[R_34 [0 0 0.325]'; ident];
 
 R_45=Y(90);
 iTj_0(:, :, 5)=[R_45 [0.095 0 0]'; ident];
 
 iTj_0(:, :, 6)=[eye(3) [0 0 0.095]'; ident];
 
-iTj_0(:, :, 7)=[eye(3) [0 0 0.345]'; ident];
+iTj_0(:, :, 7)=[eye(3) [0 0 0.355]'; ident];
 
 end
 
 
-function [iTj_0] = BuildTree_paro()
-    % This function should build the tree of frames for the chosen manipulator.
-    % Inputs: 'None'
-    % Outputs: The tree of frames.
-    
-    % iTj_0 corresponds to the trasformation from the frame <i> to <i'> which
-    % for q = 0 is equal to the trasformation from <i> to <i+1>
-    % (see notes)
-    
-    % iTj_0 is a 3-dimensional matlab matrix, suitable for defining tree of
-    % frames. iTj_0 should represent the transformation matrix between the i-th and j-th
-    % frames. iTj_0(row,col,joint_idx)
-    
-    i = [1 0 0]';
-    j = [0 1 0]';
-    k = [0 0 1]';
-    
-    iTj_0(:, :, 1) = tFactory([i j k], k.*0.105);   % <1>
-    
-    iTj_0(:, :, 2) = tFactory([i -k j], k.*0.110);  % <2>
-    
-    iTj_0(:, :, 3) = tFactory([i k -j], -j.*0.100); % <3>
-    
-    iTj_0(:, :, 4) = tFactory([i -k j], k.*0.325);  % <4>
-    
-    iTj_0(:, :, 5) = tFactory([i k -j], -j.*0.095); % <5>
-    
-    iTj_0(:, :, 6) = tFactory([i j k], k.*0.095);   % <6>
-    
-    iTj_0(:, :, 7) = tFactory([i j k], k.*0.355);   % <7> = <EE>
-
-end
+% function [iTj_0] = BuildTree()
+%     % This function should build the tree of frames for the chosen manipulator.
+%     % Inputs: 'None'
+%     % Outputs: The tree of frames.
+% 
+%     % iTj_0 corresponds to the trasformation from the frame <i> to <i'> which
+%     % for q = 0 is equal to the trasformation from <i> to <i+1>
+%     % (see notes)
+% 
+%     % iTj_0 is a 3-dimensional matlab matrix, suitable for defining tree of
+%     % frames. iTj_0 should represent the transformation matrix between the i-th and j-th
+%     % frames. iTj_0(row,col,joint_idx)
+% 
+%     i = [1 0 0]';
+%     j = [0 1 0]';
+%     k = [0 0 1]';
+% 
+%     iTj_0(:, :, 1) = tFactory([i j k], k.*0.105);   % <1>
+% 
+%     iTj_0(:, :, 2) = tFactory([i -k j], k.*0.110);  % <2>
+% 
+%     iTj_0(:, :, 3) = tFactory([i k -j], -j.*0.100); % <3>
+% 
+%     iTj_0(:, :, 4) = tFactory([i -k j], k.*0.325);  % <4>
+% 
+%     iTj_0(:, :, 5) = tFactory([i k -j], -j.*0.095); % <5>
+% 
+%     iTj_0(:, :, 6) = tFactory([i j k], k.*0.095);   % <6>
+% 
+%     iTj_0(:, :, 7) = tFactory([i j k], k.*0.355);   % <7> = <EE>
+% 
+% end
 
 
 
